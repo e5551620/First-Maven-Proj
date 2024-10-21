f27b26915de180763e78d5b38159a572b775da50
pradip <pradip.dhakne@gmail.com>
2024-10-21 22:30:24 +0530

    second comit

--- src/test/java/Test/TestWaits.java
+++ src/test/java/Test/TestWaits.java
@@ -8,29 +8,23 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
 
 public class TestWaits {
 	
-	public static void main(String args[]){
-		
-		WaitsExample();
+	@Test
+	public void method1(){
+	System.out.println("Testcae1");
 	}
 	
-	public static void WaitsExample(){
-		WebDriver driver = new ChromeDriver();
-		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		driver.get("https://www.facebook.com/");
-		WebElement uname = driver.findElement(By.id("emailxxx"));
-		WebElement pwd = driver.findElement(By.id("pass"));
-		WebElement btn = driver.findElement(By.name("login"));
-		
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		WebElement u = wait.until(ExpectedConditions.visibilityOf(uname));
-		//u.sendKeys("as@gmail.com");
-		
-		
-		driver.close();
+	@Test
+	public void method2(){
+	System.out.println("Testcae2");
 	}
-
+	
+	@Test
+	public void method3(){
+	System.out.println("Testcae3");
+	}
 }
